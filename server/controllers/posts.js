af6ec00d16f0190d9305c3b325473f5653328ccf
John Robinson <johnarobinson133@gmail.com>
2021-05-05 00:19:32 -0600

    changed server to use .env file

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,21 +1,139 @@
-import express from "express";
-import mongoose from "mongoose";
+// import express from "express";
+// import mongoose from "mongoose";
 
-import PostMessage from "../models/postMessage.js";
+// import PostMessage from "../models/postMessage.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-export const getPosts = async (req, res) => {
+// export const getPosts = async (req, res) => {
+//   try {
+//     const postMessages = await PostMessage.find();
+
+//     res.status(200).json(postMessages);
+//   } catch (error) {
+//     res.status(404).json({ message: error.message });
+//   }
+// };
+
+// export const getPost = async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     const post = await PostMessage.findById(id);
+
+//     res.status(200).json(post);
+//   } catch (error) {
+//     res.status(404).json({ message: error.message });
+//   }
+// };
+
+// export const createPost = async (req, res) => {
+//   const {
+//     title,
+//     message,
+//     price,
+//     quantity,
+//     selectedFile,
+//     creator,
+//     tags,
+//   } = req.body;
+
+//   const newPostMessage = new PostMessage({
+//     title,
+//     message,
+//     price,
+//     quantity,
+//     selectedFile,
+//     creator,
+//     tags,
+//   });
+
+//   try {
+//     await newPostMessage.save();
+
+//     res.status(201).json(newPostMessage);
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// };
+
+// export const updatePost = async (req, res) => {
+//   const { id } = req.params;
+//   const {
+//     title,
+//     message,
+//     creator,
+//     price,
+//     quantity,
+//     selectedFile,
+//     tags,
+//   } = req.body;
+
+//   if (!mongoose.Types.ObjectId.isValid(id))
+//     return res.status(404).send(`No post with id: ${id}`);
+
+//   const updatedPost = {
+//     creator,
+//     title,
+//     message,
+//     price,
+//     quantity,
+//     tags,
+//     selectedFile,
+//     _id: id,
+//   };
+
+//   await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+//   res.json(updatedPost);
+// };
+
+// export const deletePost = async (req, res) => {
+//   const { id } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(id))
+//     return res.status(404).send(`No post with id: ${id}`);
+
+//   await PostMessage.findByIdAndRemove(id);
+
+//   res.json({ message: "Post deleted successfully." });
+// };
+
+// export const likePost = async (req, res) => {
+//   const { id } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(id))
+//     return res.status(404).send(`No post with id: ${id}`);
+
+//   const post = await PostMessage.findById(id);
+
+//   const updatedPost = await PostMessage.findByIdAndUpdate(
+//     id,
+//     { likeCount: post.likeCount + 1 },
+//     { new: true }
+//   );
+
+//   res.json(updatedPost);
+// };
+
+// export default router;
+// asdf
+
+const express = require("express");
+const mongoose = require("mongoose");
+const PostMessage = require("../models/postMessage.js");
+
+const getPosts = async (req, res) => {
   try {
-    const postMessages = await PostMessage.find();
+    const post = await PostMessage.find();
 
-    res.status(200).json(postMessages);
+    res.status(200).json(post);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-export const getPost = async (req, res) => {
+const getPost = async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -27,7 +145,7 @@ export const getPost = async (req, res) => {
   }
 };
 
-export const createPost = async (req, res) => {
+const createPost = async (req, res) => {
   const {
     title,
     message,
@@ -57,7 +175,7 @@ export const createPost = async (req, res) => {
   }
 };
 
-export const updatePost = async (req, res) => {
+const updatePost = async (req, res) => {
   const { id } = req.params;
   const {
     title,
@@ -88,7 +206,7 @@ export const updatePost = async (req, res) => {
   res.json(updatedPost);
 };
 
-export const deletePost = async (req, res) => {
+const deletePost = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id))
@@ -99,21 +217,10 @@ export const deletePost = async (req, res) => {
   res.json({ message: "Post deleted successfully." });
 };
 
-export const likePost = async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
-
-  const post = await PostMessage.findById(id);
-
-  const updatedPost = await PostMessage.findByIdAndUpdate(
-    id,
-    { likeCount: post.likeCount + 1 },
-    { new: true }
-  );
-
-  res.json(updatedPost);
+module.exports = {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
 };
-
-export default router;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,21 +1,43 @@
-import express from "express";
+// import express from "express";
 
-import {
+// import {
+//   getPosts,
+//   getPost,
+//   createPost,
+//   updatePost,
+//   likePost,
+//   deletePost,
+// } from "../controllers/posts.js";
+
+// const router = express.Router();
+
+// router.get("/", getPosts);
+// router.post("/", createPost);
+// router.get("/:id", getPost);
+// router.patch("/:id", updatePost);
+// router.delete("/:id", deletePost);
+// router.patch("/:id/likePost", likePost);
+
+// export default router;
+
+const path = require("path");
+const router = require("express").Router();
+const {
   getPosts,
   getPost,
   createPost,
   updatePost,
-  likePost,
   deletePost,
-} from "../controllers/posts.js";
-
-const router = express.Router();
+} = require("../controllers/posts.js");
 
 router.get("/", getPosts);
 router.post("/", createPost);
 router.get("/:id", getPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
 
-export default router;
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+);
+
+module.exports = router;
